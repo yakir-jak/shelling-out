da97231537050b63fe684eef11e8f1d39ae4089d
yakir-jak <root@localhost.localdomain>
2018-12-25 21:30:55 +0200

    add myscript

---
+++ Hello.sh
@@ -0,0 +1,8 @@
+#######################################
+#created by : Yakir                    #
+#purpose: say Hello to the world       #                            
+#25\12\2018: marry cristmas :)         #                             
+#Version: 0.0.1                        #            
+#######################################
+echo "*Hello World !!*"
+
---
+++ variables.sh
@@ -0,0 +1,8 @@
+##############################
+# Created By: Yakir           #
+# Purpose: Show Sys Variables #
+# Date:25\12\2018             #
+# Version: 0.0.1              #
+##############################
+echo -e "Home: $HOME \nUser: $USER \nDisplay: $DISPLAY \nTerm: $TERM \nPath: $PATH \nHistSize: $HISTSIZE \nSession_Manager: $SESSIONMANAGER \nShell $SHELL \nHostName: $HOSTNAME \nLang: $LANG \nMail: $MAIL"
+
